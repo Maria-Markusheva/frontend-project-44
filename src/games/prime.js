3f69cf7401bef8048a6a1681bad139d3ed7a952c
Maria Markusheva <mashagusakova2000@mail.ru>
2025-06-27 21:41:29 +0600

    Правки по линтеру

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,18 +1,16 @@
 import runGame from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
-
-
 const isPrime = (number) => {
   if (number < 2) {
     return false;
   }
   for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) { 
-    return false;
+    if (number % i === 0) {
+      return false;
     }
-}
- return true;
+  }
+  return true;
 };
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
